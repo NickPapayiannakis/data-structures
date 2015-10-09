5d90e00ea515177eccfe237bbec61dd0293b8565
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-10-09 19:08:24 -0400

    Completed pseudoclassical stack, attempted linkedlist, completed set.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,32 @@
 var LinkedList = function(){
   var list = {};
+
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode =  Node(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var storeHead = list.head;
+    list.head = list.tail;
+    return storeHead.value;
   };
-
+  //Fails test starting line 22.
   list.contains = function(target){
+    if(this.head.value === target){
+      return true;
+    }
+    if(this.tail.next){
+      list.contains(this.tail.next);
+    }
+    return false;
   };
 
   return list;
